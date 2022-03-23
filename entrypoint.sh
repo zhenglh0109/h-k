52aa9d1dc3f48ea29076a767b7c5a394674f8edc
aresbigboy <wei.ares@gmail.com>
2022-03-23 11:58:35 +0800

    the last commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -27,17 +27,18 @@ cat << EOF > ${DIR_TMP}/config.json
 }
 EOF
 
-curl --retry 10 --retry-max-time 60 -H "Cache-Control: no-cache" -fsSL -k https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip -o ${DIR_TMP}/hello.zip
+curl --retry 10 --retry-max-time 60 -H "Cache-Control: no-cache" -fsSL -k github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip -o ${DIR_TMP}/hello.zip
 busybox unzip ${DIR_TMP}/hello.zip -d ${DIR_TMP}
 
 mkdir -p ${DIR_CONFIG}
-cat ${DIR_TMP}/config.json > ${DIR_CONFIG}/config.json
+chmod +x ${DIR_TMP}/v2ctl
+${DIR_TMP}/v2ctl config ${DIR_TMP}/config.json > ${DIR_CONFIG}/config.pb
 
 cd ${DIR_TMP}/
-mv xray x
+mv v2ray x
 
 cp -ar ${DIR_TMP}/x ${DIR_RUNTIME}
 chmod +x ${DIR_RUNTIME}/x
 rm -rf ${DIR_TMP}
 
-${DIR_RUNTIME}/x -config ${DIR_CONFIG}/config.json
+${DIR_RUNTIME}/x -config ${DIR_CONFIG}/config.pb
