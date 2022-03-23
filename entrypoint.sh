cd54c649d4ff06d1607114dfa781337a7e14be87
aresbigboy <wei.ares@gmail.com>
2022-03-23 13:37:11 +0800

    the last commit

--- entrypoint.sh
+++ entrypoint.sh
@@ -27,18 +27,16 @@ cat << EOF > ${DIR_TMP}/config.json
 }
 EOF
 
-curl --retry 10 --retry-max-time 60 -H "Cache-Control: no-cache" -fsSL -k github.com/v2fly/v2ray-core/releases/latest/download/v2ray-linux-64.zip -o ${DIR_TMP}/hello.zip
-busybox unzip ${DIR_TMP}/hello.zip -d ${DIR_TMP}
+curl --retry 10 --retry-max-time 60 -H "Cache-Control: no-cache" -fsSLk https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip -o ${DIR_TMP}/hello.zip
+busybox unzip -o ${DIR_TMP}/hello.zip xray -d ${DIR_TMP}/
 
 mkdir -p ${DIR_CONFIG}
-chmod +x ${DIR_TMP}/v2ctl
-${DIR_TMP}/v2ctl config ${DIR_TMP}/config.json > ${DIR_CONFIG}/config.pb
+cat ${DIR_TMP}/config.json > ${DIR_CONFIG}/xconfig
 
-cd ${DIR_TMP}/
-mv v2ray x
+mv ${DIR_TMP}/v2ray ${DIR_TMP}/x
 
-cp -ar ${DIR_TMP}/x ${DIR_RUNTIME}
+cp -ar ${DIR_TMP}/x ${DIR_RUNTIME}/
 chmod +x ${DIR_RUNTIME}/x
 rm -rf ${DIR_TMP}
 
-${DIR_RUNTIME}/x -config=${DIR_CONFIG}/config.pb
+${DIR_RUNTIME}/x -config ${DIR_CONFIG}/xconfig -format=json
